477e4a688f0c211b835a161d0d0ab46443f5dc70
sanevillain <denis.ismail@code.berlin>
2024-02-26 10:42:03 +0100

    add parser struct

---
+++ parser/parser.go
@@ -0,0 +1,32 @@
+package parser
+
+import (
+	"sanevillain/go-interpreter/ast"
+	"sanevillain/go-interpreter/lexer"
+	"sanevillain/go-interpreter/token"
+)
+
+type Parser struct {
+	l *lexer.Lexer
+
+	curToken  token.Token
+	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+
+	p.nextToken() // sets peekToken to the first token
+	p.nextToken() // sets curToken to the first token
+
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	return nil
+}
