4553c198647452e8c0979e8a4b61be9f73c68466
sanevillain <denis.ismail@code.berlin>
2024-02-26 12:37:20 +0100

    add peekError helper function to the parser

--- parser/parser.go
+++ parser/parser.go
@@ -1,6 +1,7 @@
 package parser
 
 import (
+	"fmt"
 	"sanevillain/go-interpreter/ast"
 	"sanevillain/go-interpreter/lexer"
 	"sanevillain/go-interpreter/token"
@@ -104,4 +105,10 @@ func (p *Parser) curTokenIs(t token.TokenType) bool {
 
 func (p *Parser) peekTokenIs(t token.TokenType) bool {
 	return p.peekToken.Type == t
+}
+
+func (p *Parser) peekError(t token.TokenType) {
+	msg := fmt.Sprintf("expected next token to be %s, got %s instead",
+		t, p.peekToken.Type)
+	p.errors = append(p.errors, msg)
 }
