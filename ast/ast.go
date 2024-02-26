65536b8eaec510957e647bd504628d8790e0892c
sanevillain <denis.ismail@code.berlin>
2024-02-26 10:01:05 +0100

    add Program struct which will be the root node of each AST

--- ast/ast.go
+++ ast/ast.go
@@ -13,3 +13,16 @@ type Expression interface {
 	Node
 	expressionNode()
 }
+
+// Program is the root node of every AST the parser produces.
+type Program struct {
+	Statements []Statement // Every program is a series of statements.
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) <= 0 {
+		return ""
+	}
+
+	return p.Statements[0].TokenLiteral()
+}
