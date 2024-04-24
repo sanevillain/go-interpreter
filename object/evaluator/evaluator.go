e97c904ef4ebcbf4fac54af8eff1d47516526ae3
sanevillain <denis.ismail@code.berlin>
2024-04-24 13:32:22 +0200

    no need to create new booleans every time

--- object/evaluator/evaluator.go
+++ object/evaluator/evaluator.go
@@ -5,6 +5,11 @@ import (
 	"sanevillain/go-interpreter/object"
 )
 
+var (
+	TRUE  = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+)
+
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
 	// Statements
@@ -18,7 +23,7 @@ func Eval(node ast.Node) object.Object {
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
 	case *ast.Boolean:
-		return &object.Boolean{Value: node.Value}
+		return nativeBoolToBooleanObject(node.Value)
 	}
 	return nil
 }
@@ -31,4 +36,11 @@ func evalStatements(statements []ast.Statement) object.Object {
 	}
 
 	return result
+}
+
+func nativeBoolToBooleanObject(input bool) *object.Boolean {
+	if input {
+		return TRUE
+	}
+	return FALSE
 }
