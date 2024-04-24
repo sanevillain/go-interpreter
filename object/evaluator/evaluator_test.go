25f687e7a0ed263331b95f598b1c3f218b14f648
sanevillain <denis.ismail@code.berlin>
2024-04-24 13:14:35 +0200

    add initial evaluator test

---
+++ object/evaluator/evaluator_test.go
@@ -0,0 +1,45 @@
+package evaluator
+
+import (
+	"sanevillain/go-interpreter/lexer"
+	"sanevillain/go-interpreter/object"
+	"sanevillain/go-interpreter/parser"
+	"testing"
+)
+
+func TestEvalIntegerExpression(t *testing.T) {
+	testCases := []struct {
+		input    string
+		expected int64
+	}{
+		{"5", 5},
+		{"10", 10},
+	}
+	for _, tC := range testCases {
+		evaluated := testEval(tC.input)
+		testIntegerObject(t, evaluated, tC.expected)
+	}
+}
+
+func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
+	result, ok := obj.(*object.Integer)
+	if !ok {
+		t.Errorf("object is not Integer. got=%T (%+v)", obj, obj)
+		return false
+	}
+
+	if result.Value != expected {
+		t.Errorf("object has wrong value. got=%d want=%d", result.Value, expected)
+		return false
+	}
+
+	return true
+}
+
+func testEval(input string) object.Object {
+	l := lexer.New(input)
+	p := parser.New(l)
+	program := p.ParseProgram()
+
+	return Eval(program)
+}
