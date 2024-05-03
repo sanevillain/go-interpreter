e5bed45cdca12a3468804ad603086ef81fc76152
sanevillain <denis.ismail@code.berlin>
2024-05-03 15:16:23 +0200

    fix lexer bug

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -124,11 +124,11 @@ func (l *Lexer) peekChar() byte {
 }
 
 func isLetter(ch byte) bool {
-	return 'a' <= ch && ch >= 'z' || 'A' <= ch && ch >= 'Z' || ch == '_'
+	return ch >= 'a' && ch <= 'z' || ch >= 'A' && ch <= 'Z' || ch == '_'
 }
 
 func isDigit(ch byte) bool {
-	return '0' <= ch && ch <= '9'
+	return ch >= '0' && ch <= '9'
 }
 
 func isWhitespace(ch byte) bool {
