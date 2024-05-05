82c58cacd9ac5395219addbae5f9da0d6269c148
sanevillain <denis.ismail@code.berlin>
2024-05-05 15:13:33 +0200

    add evaluation support for string data types

--- object/evaluator/evaluator_test.go
+++ object/evaluator/evaluator_test.go
@@ -96,6 +96,20 @@ func TestEvalIntegerExpression(t *testing.T) {
 	}
 }
 
+func TestStringLiteral(t *testing.T) {
+	input := `"Hello World!"`
+
+	evaluated := testEval(input)
+	str, ok := evaluated.(*object.String)
+	if !ok {
+		t.Fatalf("object is not String. got=%T (%+v)", evaluated, evaluated)
+	}
+
+	if str.Value != "Hello World" {
+		t.Errorf("String has wrong value. got=%q", str.Value)
+	}
+}
+
 func TestEvalBooleanExpression(t *testing.T) {
 	testCases := []struct {
 		input    string
