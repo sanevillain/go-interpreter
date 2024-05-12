6d4d4147bf80c8dcdf0482e8c7d8e397199d51bf
sanevillain <denis.ismail@code.berlin>
2024-05-12 13:35:20 +0200

    add evaluation for hash literals

--- object/evaluator/evaluator.go
+++ object/evaluator/evaluator.go
@@ -100,6 +100,8 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 		}
 
 		return &object.Array{Elements: elements}
+	case *ast.HashLiteral:
+		return evalHashLiteral(node, env)
 	}
 	return nil
 }
@@ -172,6 +174,32 @@ func evalExpressions(exprs []ast.Expression, env *object.Environment) []object.O
 	}
 
 	return result
+}
+
+func evalHashLiteral(node *ast.HashLiteral, env *object.Environment) object.Object {
+	pairs := make(map[object.HashKey]object.HashPair)
+
+	for keyNode, valueNode := range node.Pairs {
+		key := Eval(keyNode, env)
+		if isError(key) {
+			return key
+		}
+
+		hashKey, ok := key.(object.Hashable)
+		if !ok {
+			return newError("unusable as hash key: %s", key.Type())
+		}
+
+		value := Eval(valueNode, env)
+		if isError(key) {
+			return value
+		}
+
+		hashed := hashKey.HashKey()
+		pairs[hashed] = object.HashPair{Key: key, Value: value}
+	}
+
+	return &object.Hash{Pairs: pairs}
 }
 
 func evalInfixExpression(operator string, left, right object.Object) object.Object {
